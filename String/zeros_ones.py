cbd142fbf713b27ded3d2cef25f660bcd302217a
mohankumar17 <kothalamohankumar17@gmail.com>
2021-02-27 20:48:10 +0530

    Replaced c with ans

--- String/zeros_ones.py
+++ String/zeros_ones.py
@@ -3,7 +3,7 @@ s='01001100011101010011'
 
 zc=0
 oc=0
-c=0
+ans=0
 
 for i in range(len(s)):
     if s[i]=='0':
@@ -11,6 +11,6 @@ for i in range(len(s)):
     if s[i]=='1':
         oc=oc+1
     if zc==oc:
-        c=c+1
+        ans=ans+1
 
-print(c)
+print(ans)
